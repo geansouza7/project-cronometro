9ec618a6e87ab58073b985ee85933649146cd620
geansouza7 <geanshow10@gmail.com>
2023-11-13 22:47:00 -0300

    project-cronometro

---
+++ scripts.js
@@ -0,0 +1,33 @@
+let minutos = 0;
+let segundos = 0;
+let cronometro;
+
+function atualizarCronometro() {
+    segundos++;
+    if (segundos == 60) {
+        segundos = 0;
+        minutos++
+    }
+    document.getElementById('minutos').innerText = minutos.toString().padStart(2, '0');
+    document.getElementById('segundos').innerText = segundos.toString().padStart(2, '0');
+}
+
+document.getElementById('iniciar').addEventListener('click', function () {
+    if (!cronometro) {
+        cronometro = setInterval(atualizarCronometro, 1000);
+    }
+});
+
+document.getElementById('parar').addEventListener('click', function () {
+    clearInterval(cronometro);
+    cronometro = undefined;
+});
+
+document.getElementById('zerar').addEventListener('click', function () {
+minutos = 0;
+segundos = 0;
+document.getElementById('minutos').innerText = '00';
+document.getElementById('segundos').innerText = '00';
+clearInterval(cronometro);
+cronometro = undefined;
+});
